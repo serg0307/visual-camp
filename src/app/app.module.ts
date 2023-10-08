2690e54d49029ec9afd6c122f2e22e253fc65cbe
serg0307 <p14@bk.ru>
2023-10-08 15:13:14 +0300

    loader fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { FrontPageComponent } from './shared/front-page/front-page.component';
 import { MainMenuComponent } from './shared/main-menu/main-menu.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { ToolbarComponent } from './shared/toolbar/toolbar.component';
-import { GalleryComponent } from './shared/gallery/gallery.component';
+
 import { GalleryModule } from './modules/gallery/gallery.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ContactsPageComponent } from './shared/contacts-page/contacts-page.component';
@@ -20,7 +20,6 @@ import { ProductModule } from './modules/product/product.module';
     MainMenuComponent,
     FooterComponent,
     ToolbarComponent,
-    GalleryComponent,
     ContactsPageComponent
   ],
   imports: [
