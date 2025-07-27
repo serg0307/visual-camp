ce40e2510fa44fa9ba7b158d92d4b83feaeff5f9
serg0307 <p14@bk.ru>
2025-07-27 15:49:46 +0300

    touch

--- src/app/modules/workflow/stages/stage-visual/stage-visual.component.ts
+++ src/app/modules/workflow/stages/stage-visual/stage-visual.component.ts
@@ -8,13 +8,28 @@ import { WorkflowService } from 'src/app/services/workflow.service';
   selector: 'app-stage-visual',
   templateUrl: './stage-visual.component.html',
   styleUrls: ['./stage-visual.component.scss'],
-  })
+})
 export class StageVisualComponent {
   @Input() stage: WorkflowStage = <WorkflowStage>{};
   @Input() index = 0;
   widthOld = 50;
   isDragging = false;
-  constructor(private workflow: WorkflowService) {}
+  constructor(private workflow: WorkflowService) { }
+  logTouchstart(event: TouchEvent) {
+    event.preventDefault();
+    this.widthOld = event.touches[0].clientX;
+    this.isDragging = true;
+
+  }
+  logTouchend(event: TouchEvent) {
+    this.isDragging = false;
+  }
+  logTouchMove(event: TouchEvent) {
+    if (this.isDragging) {
+      this.widthOld = event.touches[0].clientX;
+      console.log(event.touches[0].clientX);
+    }
+  }
   select(item: WorkflowItem) {
     this.stage.completed = true;
     this.workflow.saveStage(this.stage);
